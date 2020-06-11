3e103856b70a3ad90066436aca7e04c4dae878b2
Otkoge <github@otkoge.hu>
2020-06-11 17:03:04 +0200

    allow all hosts for easy use

--- containAdmin/containAdmin/settings.py
+++ containAdmin/containAdmin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-1g4l@8#ir)0iat%gq)cgg-mrv@m8z#tv#3hm_w++#8(+h&n1s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
